7d8507d049acecfe0df4d373cee414e5d040c506
parth <>
2023-06-10 21:58:37 +0530

    1st draft of the python version

--- gui_interface.py
+++ gui_interface.py
@@ -4,26 +4,70 @@ import chess.pgn as pgn
 import chess.svg as svg
 import tkinter as tk
 from tkinter import filedialog
-from time import sleep
+from time import sleep, time
 import pygame
 from PIL import ImageTk, Image
 from cairosvg import svg2png
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+from io import StringIO
+import shutil
+import os
+from scipy.signal import butter, lfilter, firls
 
 fname=''
 sound=None
-board=None
+board=chess.Board()
 moves=None
+tone_dur=None
 Fs = 44100  # sampling frequency
-dur = 250  # note duration in milliseconds
+dur = 420  # note duration in milliseconds
 
-def create_tone(freq,dur,Fs,A):
-# generate appropriate major chord with base frequency
-    samples = int(Fs/freq)
-    cycles = int(freq*dur/1000)
-    triagTable = A*np.hstack((np.linspace(1,-1,int(samples/2)),np.linspace(-1,1,int(samples/2))))
-    t = np.linspace(0,dur*0.001,len(triagTable)*cycles)
-    x = np.tile(triagTable,cycles)
-    return x
+def butter_lowpass(cutoff, fs, order=2):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def apply_lowpass_filter(data, cutoff, fs, order=2):
+    #b, a = butter_lowpass(cutoff, fs, order=order)
+    b = firls(numtaps=order,fs=fs,bands=[0,cutoff],desired=[1,0.1])
+    a = [1]
+    filtered_data = lfilter(b, a, data)
+    return filtered_data
+
+def create_detuned_pulse(freq, dur, Fs, A, num_oscillators, minor):
+    # Generate multiple detuned pulse wave oscillators
+    tones = []
+    if num_oscillators==2:
+        detune_factor = [1,3/2]
+    elif num_oscillators==3:
+        detune_factor = [1,5/4*(1-minor) + 6/5*minor,3/2]
+    elif num_oscillators==4:
+        detune_factor = [1,5/4*(1-minor) + 6/5*minor,3/2,15/8*(1-minor) + 7/4*minor]
+    else:
+        detune_factor = [1,5/4*(1-minor) + 6/5*minor,3/2]
+            
+    for i in range(num_oscillators):
+        detuned_freq = freq*detune_factor[i]                            # Generate harmonics
+        
+        # Generate pulse wave oscillator
+        samples = int(Fs / detuned_freq)
+        cycles = int(detuned_freq*dur/1000)
+        #pulse_table = A/num_oscillators*(np.hstack((np.ones(samples//2), -1*np.ones(samples//2))))
+        triagTable = A/num_oscillators*np.hstack((np.linspace(1,-1,int(samples/2)),np.linspace(-1,1,int(samples/2))))
+        pulse = np.tile(triagTable, cycles)
+        tones.append(pulse)
+    # Combine the detuned oscillators
+    tone = np.zeros(np.max([i.shape for i in tones]))
+    for i in range(num_oscillators):
+        tone[:tones[i].shape[0]] += tones[i]
+    cutoff_frequency = 6000  # Adjust as desired
+    filter_order = 13  # Adjust as desired
+    
+    # Apply the low-pass filter
+    filtered_audio = apply_lowpass_filter(tone,cutoff_frequency,Fs,filter_order)
+    return filtered_audio
 
 def parse_chess_board(fname,Fs,dur):
     # setup parameters
@@ -39,12 +83,15 @@ def parse_chess_board(fname,Fs,dur):
     board = game.board()
     moves = list(game.mainline_moves())
     pgn_moves = []
-    for move in moves:
+    for ind,move in enumerate(moves):
         san_move = board.san(move)
         pgn_moves.append(san_move)
         board.push(move)
+        svg_data = svg.board(board, size=200)
+        svg2png(bytestring=svg_data, write_to=f'./.gallery/board_{ind}.png')
     
     tune=np.array([])
+    tone_dur = np.zeros(len(moves))
     for ind,move in enumerate(pgn_moves):
         rank=0
         if move[0] in board_piece:                      # piece move like Nf6, Ndf6, Nxf6, or Ndxf6
@@ -72,7 +119,8 @@ def parse_chess_board(fname,Fs,dur):
         if move[-1] in board_misc:                      # if check or mate (mate is also check) Nd6+ or Nd6#
             rank -= 12                              # half the freq if check
         tune_freq = base_freq*(2**((rank-1)/12))
-        note = create_tone(tune_freq,dur,Fs,1)
+        #note = create_tone(tune_freq,dur,Fs,1,waveform='pulse',duty_cycle=0.7)
+        note = create_detuned_pulse(freq=tune_freq, dur=dur, Fs=Fs, A=1, num_oscillators=3, minor=0)
         
         # Overlap the note with the previous note using crossfade
         if ind > 0:
@@ -80,7 +128,8 @@ def parse_chess_board(fname,Fs,dur):
             tune[-crossfade_samples:] *= np.linspace(1.0, 0.0, crossfade_samples)  # Fade out the end of the previous note
             note[:crossfade_samples] *= np.linspace(0.0, 1.0, crossfade_samples)  # Fade in the beginning of the current note
         tune = np.concatenate((tune, note))
-    return tune,game,board,moves
+        tone_dur[ind] = note.shape[0]/Fs*1000
+    return tune,tone_dur,game,board,moves
 
 # prompt for filepath
 def get_filepath():
@@ -90,15 +139,16 @@ def get_filepath():
 
 # parse the board and generate pygame handles
 def exec_parse_board():
-    global fname, sound, board, moves, Fs, dur, text
+    global fname, sound, board, moves, Fs, dur, tone_dur, text
     text.delete(1.0,tk.END)
-    text.insert(tk.END,"Parsing...")
-    
+    text.insert(tk.END,'Parsing...')
+    if not os.path.isdir('./.gallery'):
+        os.makedirs('./.gallery')                        # make a hidden directory if not present
     try:
         if not fname:
-            raise ValueError("No file selected.")
+            raise ValueError('No file selected.')
         
-        tune, game, board, moves = parse_chess_board(fname, Fs, dur)
+        tune, tone_dur, game, board, moves = parse_chess_board(fname, Fs, dur)
         pygame.init()
         pygame.mixer.init(frequency=Fs, size=-16, channels=2, buffer=4096)
         tnote = np.outer(tune, [1, 1])
@@ -106,28 +156,26 @@ def exec_parse_board():
         tnote = tnote.astype(np.int16)
         sound = pygame.sndarray.make_sound(tnote)
     except ValueError as e:
-        print("Error:", str(e))
-    except Exception as e:
-        print("An error occurred:", str(e))
-    text.insert(tk.END,"Done!")
+        print('Error:', str(e))
+    text.insert(tk.END,'Done!')
 
 # play the sound and show the board
 def play_board():
-    global sound,board, moves, dur, image_widget
+    global sound, board, moves, dur, image_widget, tone_dur
     if sound is not None:
-        sound.play()
         board.reset()
-        for move in moves:
-            svg_data = svg.board(board,size=200)
-            image_widget.delete("all")
-            svg2png(bytestring=svg_data,write_to='board.png')
-            image = Image.open('board.png')
+        svg_data = svg.board(board, size=200)
+        for ind,move in enumerate(moves):
+            board.push(move)
+            if not ind: sound.play()                # start playing at the first move only
+            image_widget.delete('all')
+            image = Image.open(f'./.gallery/board_{ind}.png')
             tk_image = ImageTk.PhotoImage(image)
             image_widget.create_image(0, 0, anchor=tk.NW, image=tk_image)
-            ''' show image in image widget on the gui '''
             image_widget.update()
-            sleep(dur/1000)
-            board.push(move)
+            sleep(tone_dur[ind]/1000)
+            svg_data = svg.board(board, size=200)
+
 
 # Create a function to quit the program
 def quit_program():
@@ -135,38 +183,40 @@ def quit_program():
     window.quit()
     window.destroy()
     sound.stop()
+    if os.path.isdir('./.gallery'):
+        shutil.rmtree('./.gallery')
 
 # Create the main window
 window = tk.Tk()
 
 # Set the window title
-window.title("File Selection")
+window.title('File Selection')
 
 # Set the window size
-window.geometry("640x400")  # Width: 600 pixels, Height: 400 pixels
+window.geometry('640x400')  # Width: 600 pixels, Height: 400 pixels
 
 # Create a button to open a file dialog
-button = tk.Button(window, text="Select File", command=get_filepath)
+button = tk.Button(window, text='Select File', command=get_filepath)
 button.grid(row=0, column=0, padx=10, pady=0, sticky='w')
 
 # Create a button to parse the board
-button2 = tk.Button(window, text="Parse", command=exec_parse_board)
+button2 = tk.Button(window, text='Parse', command=exec_parse_board)
 button2.grid(row=1, column=0, padx=10, pady=0, sticky='w')
 
 # Create a button to play
-button3 = tk.Button(window, text="Play", command=play_board)
+button3 = tk.Button(window, text='Play', command=play_board)
 button3.grid(row=2, column=0, padx=10, pady=0, sticky='w')
 
 # Create a button to quit the program
-quit_button = tk.Button(window, text="Quit", command=quit_program)
+quit_button = tk.Button(window, text='Quit', command=quit_program)
 quit_button.grid(row=3, column=0, padx=10, pady=0, sticky='w')
 
 # Create a text widget to display text content
 text = tk.Text(window, height=1, width=20)
 text.grid(row=0, column=1, padx=0, pady=0)
 
-# Create an image widget to display the SVG image
-image_widget = tk.Canvas(window, width=200, height=200, bg="white")
+# Create an image widget to display the PNG image
+image_widget = tk.Canvas(window, width=200, height=200, bg='white')
 image_widget.grid(row=1, column=1, padx=10, pady=0)
 
 # Configure grid weights to make the text widget expand to fill the available space
